abb2ff1596b2b5d15db89483f455e15aacec9865
Paloma Fernandez Jaen <palomafj91@gmail.com>
2025-04-30 00:32:31 +0200

    iconos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 function Header() {
   return (
     <header>
-      <h1>📚 MI BIBLIOTECA PERSONAL</h1>
-      <p>
-        Mis libros, mis relfexiones, mis ideas, mi vida...
+      <h1>📚 TU BIBLIOTECA PERSONAL 📚</h1>
+      <p>Aquí podras tus libros preferidos y anotar las ideas, relfexiones, y sentimientos que encontraste en ellos.
+        Además también podrás guardar los libros pendientes por leer.
       </p>
+      <h2>Tus libros, tus, ideas, tus reflexiones, tu vida...</h2>
     </header>
   );
 }
